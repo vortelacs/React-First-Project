f7f14ba11231fe90efac4f6bfd0d512b3874b79a
Eugen <vortelacs@gmail.com>
2021-05-20 02:23:49 +0300

    First JS Ex

--- js/script.js
+++ js/script.js
@@ -2,18 +2,19 @@
 const numberOfFilms = prompt("How many movies have you seen?");
 let personalMovieDB = {
     count: numberOfFilms,
-    movies:null,
-    actors:null,
-    genres:null,
+    movies: {},
+    actors: {},
+    genres: [],
     privat:false
-}
+};
 
-const fMovie = prompt("What was your last movie ?")
-const fMark = prompt("What score do you give it ?")
-const sMovie = prompt("What was your last movie ?")
-const sMark = prompt("What score do you give it ?")
+const fMovie = prompt("What was your last movie ?"),
+      fMark = prompt("What score do you give it ?"),
+      sMovie = prompt("What was your last movie ?"),
+      sMark = prompt("What score do you give it ?");
 
-const movies={
-    'fMovie' : 'fMark',
-    'sMovie' : 'sMark'
-}
+
+personalMovieDB.movies[fMovie] = fMark;
+personalMovieDB.movies[sMovie] = sMark;
+    
+console.log(personalMovieDB.movies);

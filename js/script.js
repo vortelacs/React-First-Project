437158ef62b847668e78f9fec44ca7afad5640e1
Eugen <vortelacs@gmail.com>
2021-05-20 02:11:57 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 "use strict";
+const numberOfFilms = prompt("How many movies have you seen?");
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies:null,
+    actors:null,
+    genres:null,
+    privat:false
+}
 
-const str = "dds";
+const fMovie = prompt("What was your last movie ?")
+const fMark = prompt("What score do you give it ?")
+const sMovie = prompt("What was your last movie ?")
+const sMark = prompt("What score do you give it ?")
 
-console.log(`dd${str}`);
-
+const movies={
+    'fMovie' : 'fMark',
+    'sMovie' : 'sMark'
+}

d26a5ced63e55fb60b70c593c1fbfe672feb768c
Eugen <vortelacs@gmail.com>
2021-05-20 13:46:13 +0300

    Adding functions

--- js/script.js
+++ js/script.js
@@ -1,20 +1,30 @@
 "use strict";
 const numberOfFilms = prompt("How many movies have you seen?");
+if (numberOfFilms <= 10 && numberOfFilms > 0) { alert("Not so much"); }
+else if (numberOfFilms < 30 && numberOfFilms > 10) { alert("OK"); }
+else if (numberOfFilms >= 30) { alert("Pretty much"); }
+else { alert("Error"); }
+
 let personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
-    privat:false
+    privat: false
 };
 
-const fMovie = prompt("What was your last movie ?"),
-      fMark = prompt("What score do you give it ?"),
-      sMovie = prompt("What was your last movie ?"),
-      sMark = prompt("What score do you give it ?");
+for (let i = 0; i < 2; i++) {
+    let mark;
+    const movie = prompt("What was your last movie ?");
+    if(movie.length < 2 || movie.length > 50){
+        i--;
+        continue;
+    }
+    do{
+        mark = prompt("What score do you give it ?");
+    }
+    while(mark.length < 2);
 
-
-personalMovieDB.movies[fMovie] = fMark;
-personalMovieDB.movies[sMovie] = sMark;
-    
+    personalMovieDB.movies[movie] = mark;
+}
 console.log(personalMovieDB.movies);
